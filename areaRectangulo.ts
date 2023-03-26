3f2ee5fea6433ce14161e6c518cc79816907a797
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-03-26 18:25:03 -0300

    new

--- areaRectangulo.ts
+++ areaRectangulo.ts
@@ -1,5 +1,5 @@
 import * as readlineSync from "readline-sync";
-let base : number = readlineSync.questionInt( "Ingrese la base: ");
-let altura : number = readlineSync.questionInt( "Ingrese la altura: ");
-let area : number = base * altura;
+let base : number = readlineSync.questionInt( "Ingrese la base: 3");
+let altura : number = readlineSync.questionInt( "Ingrese la altura: 2");
+let area : number = 3 * 2;
 console.log("El área es: ", area);
