d3c2a21f68e6e1e4ffd76cec303756f1db03a65d
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-03-26 18:50:15 -0300

    new

--- areaRectangulo.ts
+++ areaRectangulo.ts
@@ -1,4 +1,4 @@
-import * as readlineSync from ‘readline-sync’;
+import * as readlineSync from "readline-sync";
 let base : number = readlineSync.questionInt( "Ingrese la base: ");
 let altura : number = readlineSync.questionInt( "Ingrese la altura: ");
 let area : number = base * altura;
