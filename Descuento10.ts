ab29e16c439fda428904c052c40cc4e585efaba6
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-03-26 19:17:47 -0300

    new

--- Descuento10.ts
+++ Descuento10.ts
@@ -2,4 +2,5 @@ import * as readlineSync from "readline-sync";
 let precio : number = readlineSync.questionInt( "Ingrese la precio: ");
 let porcentaje : number = readlineSync.questionInt( "Ingrese la porcentaje: 10");
 let Total : number = precio * porcentaje / 100;
-console.log("El Total es: ", Total:);
+console.log("El Total es: ", Total);
+
